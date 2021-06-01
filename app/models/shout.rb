6e2cf7f8cc202fac2e4bf1b5f1a8b1838f753f84
Nur-ayn Hashim <nur-ayn.hashim@prodigyfinance.com>
2021-06-01 09:46:43 +0200

    added user and shout page

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -2,4 +2,6 @@ class Shout < ApplicationRecord
   belongs_to :user
   validates :body, presence: true, length: { in: 1..144 }
   validates :user, presence: true
+  delegate :username, to: :user
+
 end
