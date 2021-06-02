a095c5b8a5cddb8467679ab0bc1c08a6a6ea6013
Nur-ayn Hashim <nur-ayn.hashim@prodigyfinance.com>
2021-06-02 15:43:05 +0200

    added liking and unliking

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -3,4 +3,12 @@ module ShoutHelper
         email_digest = Digest::MD5.hexdigest(user.email)
          image_tag "//www.gravatar.com/avatar/#{email_digest}"
     end
+
+    def like_button(shout)
+        if current_user.liked?(shout)
+            link_to "unlike", unlike_shout_path(shout), method: :delete
+        else
+            link_to "like", like_shout_path(shout), method: :post
+        end
+    end
 end
